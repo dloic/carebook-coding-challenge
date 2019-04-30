3cce7edea9a598ab0fedbfcefbfd3161d77259fe
shahabc <shahab74@gmail.com>
2019-04-30 12:07:57 -0400

    fixed UI Tests

--- CareBookChallenge/CareBookChallengeUITests/CareBookChallengeUITests.swift
+++ CareBookChallenge/CareBookChallengeUITests/CareBookChallengeUITests.swift
@@ -13,27 +13,29 @@ class CareBookChallengeUITests: XCTestCase {
     func testAppFlow() {
         
         let app = XCUIApplication()
+        app.launchArguments = ["enable-testing"]
+        app.launch()
         
         //Close Onboarding
         let onboardingDoneButton = app.navigationBars["CareBookChallenge.OnBoardingVC"].buttons["Done"]
-        XCTAssert(onboardingDoneButton.exists)
+        XCTAssert(onboardingDoneButton.waitForExistence(timeout: 10))
         onboardingDoneButton.tap()
         
         //Click First Cell
         let cellCount = app.tables.cells.count
         if cellCount != 0 {
             let firstCell = app.tables.element(boundBy: 0).cells.element(boundBy: 0)
-            XCTAssert(firstCell.exists)
+            XCTAssert(firstCell.waitForExistence(timeout: 10))
             firstCell.tap()
         }
         
         //Check first name Label
-        let firstNameLabel = app.otherElements["firstName"]
-        XCTAssert(firstNameLabel.exists)
+        let firstNameLabel = app.staticTexts["firstName"]
+        XCTAssert(firstNameLabel.waitForExistence(timeout: 10))
         
         //Go Back to List View
         let backButton = app.navigationBars["CareBookChallenge.PatientDetailVC"].buttons["Back"]
-        XCTAssert(backButton.exists)
+        XCTAssert(backButton.waitForExistence(timeout: 10))
         backButton.tap()
 
     }
