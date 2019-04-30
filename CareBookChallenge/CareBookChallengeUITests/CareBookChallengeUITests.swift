e209398535e1a93576fc19fdd1badb0b5c24e119
shahabc <shahab74@gmail.com>
2019-04-30 11:55:08 -0400

    Added ui tests and updated the readme file

--- CareBookChallenge/CareBookChallengeUITests/CareBookChallengeUITests.swift
+++ CareBookChallenge/CareBookChallengeUITests/CareBookChallengeUITests.swift
@@ -10,25 +10,32 @@ import XCTest
 
 class CareBookChallengeUITests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAppFlow() {
+        
+        let app = XCUIApplication()
+        
+        //Close Onboarding
+        let onboardingDoneButton = app.navigationBars["CareBookChallenge.OnBoardingVC"].buttons["Done"]
+        XCTAssert(onboardingDoneButton.exists)
+        onboardingDoneButton.tap()
+        
+        //Click First Cell
+        let cellCount = app.tables.cells.count
+        if cellCount != 0 {
+            let firstCell = app.tables.element(boundBy: 0).cells.element(boundBy: 0)
+            XCTAssert(firstCell.exists)
+            firstCell.tap()
+        }
+        
+        //Check first name Label
+        let firstNameLabel = app.otherElements["firstName"]
+        XCTAssert(firstNameLabel.exists)
+        
+        //Go Back to List View
+        let backButton = app.navigationBars["CareBookChallenge.PatientDetailVC"].buttons["Back"]
+        XCTAssert(backButton.exists)
+        backButton.tap()
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
 }
